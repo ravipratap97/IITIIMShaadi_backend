0249feaf8e62e1e86ea817eadb44e1e8e0aad4d1
Akshith Banda <Akshith@s7works.io>
2020-09-25 20:02:13 +0530

    one to many relation models

--- dataModels/main.go
+++ dataModels/main.go
@@ -23,6 +23,17 @@ type (
 		Gender string `json:"gender"`
 		Mobile string `json:"mobile_no"`
 		//DOB    time.Time.Date() `json:"birth_date"`
+		UserId int `gorm:"primaryKey",json:"id"`
+
+		Friends []Friend `gorm:"foreignkey:UserID;references:UserId"`
+
+		SentChats     []SentChat     `gorm:"foreignkey:UserID;references:UserId"`
+		ReceivedChats []ReceivedChat `gorm:"foreignkey:UserID;references:UserId"`
+		Subscriptions []Subscription `gorm:"foreignkey:UserID;references:UserId"`
+
+		Albums         []Album         `gorm:"foreignkey:UserID;references:UserId"`
+		FolderListings []FolderListing `gorm:"foreignkey:UserID;references:UserId"`
+		Contacts       []Contact       `gorm:"foreignkey:UserID;references:UserId"`
 	}
 	//Profile of user
 	Profile struct {
@@ -41,6 +52,7 @@ type (
 		Smoke      string `json:"smoke"`
 		HealthIss  string `json:"health_issue"`
 		AboutMe    string `json:"about_me"`
+		UserID     int
 	}
 	//Family details of user
 	Family struct {
@@ -50,6 +62,7 @@ type (
 		MotherOccp string `json:"mother_occupation"`
 		Brother    string `json:"brother"`
 		Sister     string `json:"sister"`
+		UserID     int
 	}
 	//Education details of user
 	Education struct {
@@ -62,6 +75,7 @@ type (
 		Grad        string `json:"graduation"`
 		SchoolYr    int    `json:"schooling_year"`
 		School      string `json:"schooling"`
+		UserID      int
 	}
 	//Job details of user
 	Job struct {
@@ -70,6 +84,7 @@ type (
 		JobLoc   string `json:"job_location"`
 		LinkedIn string `json:"linked_in"`
 		IncomeAn string `json:"annual_income"`
+		UserID   int
 	}
 	//Address details of user
 	Address struct {
@@ -83,6 +98,7 @@ type (
 		CurrState string `json:"current_state"`
 		CurrCntry string `json:"current_country"`
 		CurrZipCd string `json:"current_zipcode"`
+		UserID    int
 	}
 	//Other details regarding user
 	Other struct {
@@ -90,6 +106,7 @@ type (
 		BrideChe string `json:"choice_of_bride"`
 		GroomChe string `json:"choice_of_groom"`
 		PrflCmpl int    `json:"profile_complition"`
+		UserID   int
 	}
 	//PartnerChoice preferred by user
 	PartnerChoice struct {
@@ -103,6 +120,7 @@ type (
 		PartHtMax  string `json:"prefered_partner_height_max"`
 		PartHtMin  string `json:"prefered_partner_height_min"`
 		PartMartSt bool   `json:"prefered_partner_marital_status"`
+		UserID     int
 	}
 	//EmailData of user
 	EmailData struct {
@@ -129,6 +147,7 @@ type (
 		EmailSentSt1 int8   `json:"emailsentstatus1"`
 		DeactEmail   int8   `json:"deactivatemail"`
 		Pemail       int8   `json:"pemail"`
+		UserID       int
 	}
 	//VerificationData for user
 	VerificationData struct {
@@ -137,6 +156,109 @@ type (
 		BiodataStatus int8   `json:"biodata_status"`
 		IdProofStatus int8   `json:"identity_proof_verified"`
 		//DocVerified  int8 	`json:"document_verified"`
+		UserID int
+	}
+	//Friend of user
+	Friend struct {
+		gorm.Model
+		FriendId     int `json:"id"`
+		ShortListed  bool
+		RequestFrnd  bool
+		InviteedFrnd bool
+		UserID       int
+	}
+	//SentChat from user to others
+	SentChat struct {
+		ChatId        int       `json:"id"`
+		ToUserId      int       `json:"to_user_id"`
+		Message       string    `json:"message"`
+		ChatType      string    `json:"chat_type"`
+		SentTime      time.Time `json:"sent"`
+		Recd          uint      `json:"recd"`
+		LatestMsg     string    `json:"latestMessage"`
+		LatestMsgDate time.Time `json:"latestDate"`
+		MsgStatus     string    `json:"messageStatus"`
+		UserID        int
+	}
+	//ReceivedChat from others
+	ReceivedChat struct {
+		ChatId        int       `json:"id"`
+		FromUserId    int       `json:"from_user_id"`
+		Message       string    `json:"message"`
+		ChatType      string    `json:"chat_type"`
+		SentTime      time.Time `json:"sent"`
+		Recd          uint      `json:"recd"`
+		LatestMsg     string    `json:"latestMessage"`
+		LatestMsgDate time.Time `json:"latestDate"`
+		MsgStatus     string    `json:"messageStatus"`
+		UserID        int
+	}
+	//Subscription of user
+	Subscription struct {
+		SubId     int       `json:"ID"`
+		TransId   string    `json:"transaction_id"`
+		PayMode   string    `json:"payment_mode"`
+		Amount    string    `json:"amount"`
+		PayDate   time.Time `json:"payment_date"`
+		ExpDate   time.Time `json:"exp_date"`
+		Duration  string    `json:"duration"`
+		LastRenew time.Time `json:"last_renewed_on"`
+		NextRenew time.Time `json:"next_renew_on"`
+		UserID    int
+	}
+	//Album of user
+	Album struct {
+		AlbumId       int       `json:"id"`
+		OwnerType     string    `json:"owner_type"`
+		DateUpload    time.Time `json:"date_uploaded"`
+		Title         string    `json:"title"`
+		Description   string    `json:"description"`
+		Privacy       int       `json:"privacy"`
+		PicOrgUrl     string    `json:"pic_org_url"`
+		PicOrgPath    string    `json:"pic_org_path"`
+		PicMidUrl     string    `json:"pic_mid_url"`
+		PicMidPath    string    `json:"pic_mid_path"`
+		PicThumbUrl   string    `json:"pic_thumb_url"`
+		PicThumbPath  string    `json:"pic_thumb_path"`
+		ImportSts     int       `json:"import_status"`
+		OldActivityId int       `json:"old_activity_id"`
+		NewActivityId int       `json:"new_activity_id"`
+		Favoutite     bool      `json:"favorites"`
+		UserID        int
+	}
+	//Contact bu user
+	Contact struct {
+		gorm.Model
+		Viewed      int `json:"viewedContacts"`
+		Denied      int `json:"deniedUsersIds"`
+		RequestSent int `json:"allRequestSentIds"`
+		DeniedByMe  int `json:"deniedByMeUsersIds"`
+		UserID      int
+	}
+	//FolderListing of user
+	FolderListing struct {
+		ListingId int    `json:"id"`
+		MetaKey   string `json:"meta_key"`
+		MetaValue string `json:"meta_value"`
+		FrndsIds  int    `json:"friends_ids"`
+		//DateAdded time.Time `json:"date_added"`
+		UserID int
+	}
+	//SuccessStory of org
+	SuccessStory struct {
+		gorm.Model
+		Content string `json:"content"`
+		Title   string `json:"title"`
+		//DateAdded time.Time `json:"date_added"`
+	}
+	//MediaCoverage of org
+	MediaCoverage struct {
+		gorm.Model
+		Title     string `json:"title"`
+		Link      string `json:"link"`
+		Image     string `json:"image"`
+		SortOrder int    `json:"sort_order"`
+		//DateAdded time.Time `json:"date_added"`
 	}
 )
 
@@ -149,7 +271,9 @@ func main() {
 	// Migrate the schema
 	db.AutoMigrate(&User{}, &Profile{}, &Family{},
 		&Education{}, &Job{}, &Address{}, &Other{},
-		&PartnerChoice{}, &EmailData{}, &VerificationData{})
+		&PartnerChoice{}, &EmailData{}, &VerificationData{}, &Friend{},
+		&SentChat{}, &ReceivedChat{}, &Subscription{}, &Album{}, &Contact{},
+		&FolderListing{}, &SuccessStory{}, &MediaCoverage{})
 
 	jsonBody := map[string]string{"token": "12931e786a9da517f10c52880c5711eb", "userId": "9798"}
 	jsonValue, err := json.Marshal(jsonBody)
@@ -188,6 +312,7 @@ func userTable(db *gorm.DB, body []byte) {
 		Gender:   user["gender"].(string),
 		Mobile:   user["mobile_no"].(string),
 		//DOB:      user["birth_data"].(string),
+		UserId: int(userData["emailData"].(map[string]interface{})["id"].(float64)),
 	})
 }
 
@@ -210,6 +335,7 @@ func profileTable(db *gorm.DB, body []byte) {
 		Smoke:      profile["smoke"].(string),
 		HealthIss:  profile["health_issue"].(string),
 		AboutMe:    profile["about_me"].(string),
+		UserID:     int(profileData["emailData"].(map[string]interface{})["id"].(float64)),
 	})
 }
 
@@ -225,6 +351,7 @@ func familyTable(db *gorm.DB, body []byte) {
 		MotherOccp: family["mother_occupation"].(string),
 		Brother:    family["brother"].(string),
 		Sister:     family["sister"].(string),
+		UserID:     int(familyData["emailData"].(map[string]interface{})["id"].(float64)),
 	})
 }
 
@@ -243,6 +370,7 @@ func educationTable(db *gorm.DB, body []byte) {
 		Grad:        education["graduation"].(string),
 		SchoolYr:    int(education["schooling_year"].(float64)),
 		School:      education["schooling"].(string),
+		UserID:      int(educationData["emailData"].(map[string]interface{})["id"].(float64)),
 	})
 }
 
@@ -257,6 +385,7 @@ func jobTable(db *gorm.DB, body []byte) {
 		JobLoc:   job["job_location"].(string),
 		LinkedIn: job["linked_in"].(string),
 		IncomeAn: job["annual_income"].(string),
+		UserID:   int(jobData["emailData"].(map[string]interface{})["id"].(float64)),
 	})
 }
 
@@ -276,6 +405,7 @@ func addressTable(db *gorm.DB, body []byte) {
 		CurrState: address["current_state"].(string),
 		CurrCntry: address["current_country"].(string),
 		CurrZipCd: address["current_zipcode"].(string),
+		UserID:    int(addressData["emailData"].(map[string]interface{})["id"].(float64)),
 	})
 }
 
@@ -289,6 +419,7 @@ func otherTable(db *gorm.DB, body []byte) {
 		BrideChe: other["choice_of_bride"].(string),
 		GroomChe: other["choice_of_groom"].(string),
 		PrflCmpl: int(other["profile_complition"].(float64)),
+		UserID:   int(otherData["emailData"].(map[string]interface{})["id"].(float64)),
 	})
 }
 
@@ -308,6 +439,7 @@ func partnerChoiceTable(db *gorm.DB, body []byte) {
 		PartHtMax:  partnerChoice["prefered_partner_height_max"].(string),
 		PartHtMin:  partnerChoice["prefered_partner_height_min"].(string),
 		PartMartSt: partnerChoice["prefered_partner_marital_status"].(bool),
+		UserID:     int(partnerChoiceData["emailData"].(map[string]interface{})["id"].(float64)),
 	})
 }
 
@@ -340,6 +472,7 @@ func emailDataTable(db *gorm.DB, body []byte) {
 		EmailSentSt1: int8(email["emailsentstatus1"].(float64)),
 		DeactEmail:   int8(email["deactivatemail"].(float64)),
 		Pemail:       int8(email["pemail"].(float64)),
+		UserID:       int(email["id"].(float64)),
 	})
 }
 
@@ -354,5 +487,6 @@ func verificationDataTable(db *gorm.DB, body []byte) {
 		BiodataStatus: int8(verify["biodata_status"].(float64)),
 		IdProofStatus: int8(verify["identity_proof_verified"].(float64)),
 		//DocVerified: int8(verify["document_verified"].(float64)),
+		UserID: int(verificationData["emailData"].(map[string]interface{})["id"].(float64)),
 	})
 }
